09320f796dc6692d993983eab191c2ab51e43f45
gordonzhang-sbu <gordon.zhang@stonybrook.edu>
2026-04-27 14:40:43 +0000

    delete

--- README.md
+++
@@ -1,4 +0,0 @@
-# Gordon Zhang
-# 117574236
-
-For all the function in this assignment, if string contains multiple null terminator, then it will only calculate and work until it reaches the first null.
--- cse_caesar.h
+++
@@ -1,8 +0,0 @@
-#ifndef CSE_CAESAR_H
-#define CSE_CAESAR_H
-
-int encryptCaesar(const char *plaintext, char *ciphertext, int key);
-int decryptCaesar(const char *ciphertext, char *plaintext, int key);
-int hasEOM(const char *string, int index);
-
-#endif
